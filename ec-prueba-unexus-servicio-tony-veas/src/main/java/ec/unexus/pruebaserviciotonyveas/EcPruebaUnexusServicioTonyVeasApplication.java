69bb7d92489383ded6561c64b963ca5d0235d527
Tony <tveascervantes@hotmail.com>
2023-06-15 14:51:37 -0500

    TVC - 15062023 - feat: Creando estructura inicial del proyecto, para la creación del servicio usando Java 8+, Spring boot y PostgreSQL.

---
+++ ec-prueba-unexus-servicio-tony-veas/src/main/java/ec/unexus/pruebaserviciotonyveas/EcPruebaUnexusServicioTonyVeasApplication.java
@@ -0,0 +1,13 @@
+package ec.unexus.pruebaserviciotonyveas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcPruebaUnexusServicioTonyVeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcPruebaUnexusServicioTonyVeasApplication.class, args);
+	}
+
+}
---
+++ ec-prueba-unexus-servicio-tony-veas/src/test/java/ec/unexus/pruebaserviciotonyveas/EcPruebaUnexusServicioTonyVeasApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.unexus.pruebaserviciotonyveas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcPruebaUnexusServicioTonyVeasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
