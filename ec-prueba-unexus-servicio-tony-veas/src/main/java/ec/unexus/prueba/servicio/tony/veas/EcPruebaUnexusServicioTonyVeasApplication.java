4542c897a1be5464a04777a93a2ad4d53616f121
Tony <tveascervantes@hotmail.com>
2023-06-15 16:57:19 -0500

    TVC - 15062023 - feat: Configuración de conexión de base de datos

--- ec-prueba-unexus-servicio-tony-veas/src/main/java/ec/unexus/prueba/servicio/tony/veas/EcPruebaUnexusServicioTonyVeasApplication.java
+++ ec-prueba-unexus-servicio-tony-veas/src/main/java/ec/unexus/prueba/servicio/tony/veas/EcPruebaUnexusServicioTonyVeasApplication.java
@@ -1,4 +1,4 @@
-package ec.unexus.pruebaserviciotonyveas;
+package ec.unexus.prueba.servicio.tony.veas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ ec-prueba-unexus-servicio-tony-veas/src/main/java/ec/unexus/prueba/servicio/tony/veas/configuration/DatabaseConfiguration.java
@@ -0,0 +1,26 @@
+package ec.unexus.prueba.servicio.tony.veas.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfiguration {
+	private final DataSourceProperties dataSourceProperties;
+
+	public DatabaseConfiguration(DataSourceProperties dataSourceProperties) {
+		this.dataSourceProperties = dataSourceProperties;
+	}
+
+	@Bean
+	public DataSource getDataSource() {
+		return DataSourceBuilder.create()
+								.driverClassName(dataSourceProperties.getDriverClassName())
+								.url(dataSourceProperties.getUrl())
+								.username(dataSourceProperties.getUsername())
+								.password(dataSourceProperties.getPassword())
+								.build();
+	}
+}
